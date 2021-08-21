2338aa38bc9096492b514ae769ddd348ec6279c9
LiJianying <lijy91@foxmail.com>
2021-08-22 07:45:48 +0800

    Update pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:network_inspector/network_inspector.dart';
 // ignore: unused_import
 import 'package:dart_vlc/dart_vlc.dart'
     if (dart.library.html) 'utilities/dart_vlc_noop.dart' as vlc;
@@ -8,15 +7,6 @@ import './includes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  NetworkInspector(
-    onRequest: (res) {
-      print(prettyJsonString(res.toJson()));
-    },
-    onResponse: (req) {
-      print(prettyJsonString(req.toJson()));
-    },
-  );
 
   vlc.DartVLC.initialize();
 

241b51da1eee9819b678c788e5094dd4dd6200de
LiJianying <lijy91@foxmail.com>
2021-10-01 10:07:37 +0800

    Add release script

---
+++ scripts/release.dart
@@ -0,0 +1,126 @@
+import "dart:io";
+
+import 'package:archive/archive_io.dart';
+import 'package:yaml/yaml.dart';
+
+const kLinux = 'linux';
+const kMacOS = 'macos';
+const kWindows = 'windows';
+
+final Directory workDir = Directory.current;
+final Directory distDir = Directory('${workDir.path}/dist');
+final Directory buildDir = Directory('${workDir.path}/build');
+
+class ReleaseInfo {
+  final String platform;
+  final String name;
+  final String description;
+  final String buildNumber;
+  final String version;
+
+  ReleaseInfo({
+    this.platform,
+    this.name,
+    this.description,
+    this.buildNumber,
+    this.version,
+  });
+
+  factory ReleaseInfo.fromPlatform(String platform) {
+    final yamlString = File('${workDir.path}/pubspec.yaml').readAsStringSync();
+    final yamlDoc = loadYaml(yamlString);
+
+    String pubspecName = yamlDoc['name'];
+    String pubspecVersion = yamlDoc['version'];
+
+    return ReleaseInfo(
+      platform: platform,
+      name: pubspecName.split('_').first,
+      buildNumber: pubspecVersion.split('+').last,
+      version: pubspecVersion.split('+').first,
+    );
+  }
+
+  String get buildSubcommand {
+    switch (platform) {
+      case kWindows:
+        return 'windows';
+      default:
+    }
+    return null;
+  }
+
+  Directory get buildOutputDir {
+    switch (platform) {
+      case kWindows:
+        return Directory('${workDir.path}/build/windows/runner/Release');
+      default:
+    }
+    return null;
+  }
+
+  Directory get binaryArchiveDir {
+    return Directory('${distDir.path}/$version/$name-$platform-v$version');
+  }
+
+  File get binaryFile {
+    String fileExt = '';
+    switch (platform) {
+      case kMacOS:
+        fileExt = 'dmg';
+        break;
+      case kWindows:
+        fileExt = 'zip';
+        break;
+      default:
+    }
+    return File('${distDir.path}/$version/$name-$platform-v$version.$fileExt');
+  }
+}
+
+void main(List<String> args) async {
+  final String platform = args[0];
+
+  final ReleaseInfo releaseInfo = ReleaseInfo.fromPlatform(platform);
+
+  Process progress = await Process.start(
+    'flutter',
+    ['build', releaseInfo.buildSubcommand],
+    runInShell: true,
+  )
+    ..stdout.listen((event) => print(String.fromCharCodes(event).trim()))
+    ..stderr.listen((event) => print(String.fromCharCodes(event).trim()));
+
+  int exitCode = await progress.exitCode;
+  if (exitCode != 0) {
+    print('exitCode: $exitCode');
+    return;
+  }
+
+  File binaryFile = releaseInfo.binaryFile;
+
+  if (releaseInfo.binaryArchiveDir.existsSync()) {
+    releaseInfo.binaryArchiveDir.deleteSync(recursive: true);
+  }
+  releaseInfo.binaryArchiveDir.createSync(recursive: true);
+
+  if (platform == kWindows) {
+    String buildOutputPath = releaseInfo.buildOutputDir.path;
+    String binaryArchivePath = releaseInfo.binaryArchiveDir.path;
+
+    await Process.run('cp', ['-R', buildOutputPath, binaryArchivePath]);
+    await Process.run('mv', [buildOutputPath, binaryArchivePath]);
+    await Process.run('mv', [
+      '$binaryArchivePath/Release',
+      '$binaryArchivePath/${releaseInfo.name}'
+    ]);
+
+    final zipFileEncoder = ZipFileEncoder();
+    zipFileEncoder.zipDirectory(
+      releaseInfo.binaryArchiveDir,
+      filename: binaryFile.path,
+    );
+  }
+
+  releaseInfo.binaryArchiveDir.deleteSync(recursive: true);
+}

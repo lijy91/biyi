acfaee78bf84ef7e15a5d7a2d754c787337cd794
LiJianying <lijy91@foxmail.com>
2021-11-14 15:04:59 +0800

    Update tray menu (Add website & discussion)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
+import 'package:anyinspect/anyinspect.dart';
+import 'package:anyinspect_plugin_network/anyinspect_plugin_network.dart';
+import 'package:anyinspect_plugin_shared_preferences/anyinspect_plugin_shared_preferences.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_localization_loader/easy_localization_loader.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:launch_at_startup/launch_at_startup.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:window_manager/window_manager.dart';
 
 import './includes.dart';
@@ -11,14 +19,30 @@ void main() async {
   await EasyLocalization.ensureInitialized();
 
   await AudioPlayer.ensureInitialized();
-  await ProAccount.instance.ensureInitialized();
   if (kIsLinux || kIsMacOS || kIsWindows) {
     await WindowManager.instance.ensureInitialized();
   }
 
+  if (kIsMacOS || kIsWindows) {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    LaunchAtStartup.instance.setup(
+      appName: packageInfo.appName,
+      appPath: Platform.resolvedExecutable,
+    );
+  }
+
+  await ProAccount.instance.ensureInitialized();
+
   await initEnv('dev');
   await initLocalDb();
   await initConfig();
+
+  if (!kReleaseMode) {
+    AnyInspect anyInspect = AnyInspect.instance;
+    anyInspect.addPlugin(AnyInspectPluginNetwork());
+    anyInspect.addPlugin(AnyInspectPluginSharedPreferences());
+    anyInspect.start();
+  }
 
   runApp(
     EasyLocalization(
